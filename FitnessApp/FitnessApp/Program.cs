e3e60fa0a6edce8bf4c2948e21ac6a3e9df715fd
Mohammad Hanbali <mokai86@gmail.com>
2017-01-23 03:23:22 -0500

    Set up safeguards for failures in input format. Type what you want, I'M READY!!!

--- FitnessApp/FitnessApp/Program.cs
+++ FitnessApp/FitnessApp/Program.cs
@@ -17,47 +17,53 @@ namespace FitnessApp
             {
                 // Prompt user for time exercised
                 Console.Write("How many minutes did you workout for? (Type 'quit' if you whimped out, lol) ");
-                string entry = Console.ReadLine();
-
-                if (entry == "quit")
+                try
                 {
-                    keepGoing = false;
-                }
-                else
-                {
-                    // Add time to total
-                    int minutes = int.Parse(entry);
-
-                    if (minutes <=0)
+                    string entry = Console.ReadLine();
+                    if (entry == "quit")
                     {
-                        Console.WriteLine("You're a failure...");
-                        Console.WriteLine("but then again you might have just typed something wrong.");
-                        Console.WriteLine("Here try again:");
-                        continue;
-                    }
-                    else if (minutes <= 10)
-                    {
-                        Console.WriteLine("You can do better!");
-                    }
-                    else if(minutes <=30)
-                    {
-                        Console.WriteLine("Okay, now we're getting somewhere! *clap clap*");
-                    }
-                    else if(minutes <=60)
-                    {
-                        Console.WriteLine("Feel the BURN! Keep going champ!");
+                        keepGoing = false;
                     }
                     else
                     {
-                        Console.WriteLine("THIS IS SPARTA!!!");
+                        // Add time to total
+                        int minutes = int.Parse(entry);
+
+                        if (minutes <= 0)
+                        {
+                            Console.WriteLine("You're a failure...");
+                            Console.WriteLine("but then again you might have just typed something wrong.");
+                            Console.WriteLine("Here try again:");
+                            continue;
+                        }
+                        else if (minutes <= 10)
+                        {
+                            Console.WriteLine("You can do better!");
+                        }
+                        else if (minutes <= 30)
+                        {
+                            Console.WriteLine("Okay, now we're getting somewhere! *clap clap*");
+                        }
+                        else if (minutes <= 60)
+                        {
+                            Console.WriteLine("Feel the BURN! Keep going champ!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("THIS IS SPARTA!!!");
+                        }
+
+                        runningTotal = runningTotal + minutes;
+
+                        //Display total time
+                        Console.Write("You've worked out for: " + runningTotal + " minutes!");
                     }
-
-                    runningTotal = runningTotal + minutes;
-
-                    //Display total time
-                    Console.Write("You've worked out for: " + runningTotal + " minutes!");
+                    Console.WriteLine("See ya!");
                 }
-                Console.WriteLine("See ya!");
+                catch (FormatException)
+                {
+                    Console.WriteLine("That wasn't the information I asked for.");
+                }
             }            
         }
     }
