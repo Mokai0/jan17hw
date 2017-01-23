63a99661ecd38748c1417373334ca41f416f7678
Mohammad Hanbali <mokai86@gmail.com>
2017-01-23 01:57:14 -0500

    wrote basis of program

--- FitnessApp/FitnessApp/Program.cs
+++ FitnessApp/FitnessApp/Program.cs
@@ -8,8 +8,18 @@ namespace FitnessApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            int runningTotal = 0;
+
+            // Prompt user for time exercised
+            Console.Write("How many minutes did you workout for? ");
+            string entry = Console.ReadLine();
+
+            // Add time to total
+
+            //Display total time
+            Console.Write("You've worked out for: " + entry + " minutes!");
         }
     }
 }
