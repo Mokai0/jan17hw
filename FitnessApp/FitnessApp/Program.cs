d4eacf78c92d587d79f2842a1c2242a458fe527f
Mohammad Hanbali <mokai86@gmail.com>
2017-01-30 13:56:17 -0500

    Finished fitness app tutuorial!

--- FitnessApp/FitnessApp/Program.cs
+++ FitnessApp/FitnessApp/Program.cs
@@ -10,10 +10,9 @@ namespace FitnessApp
     {
         static void Main()
         {
-            double runningTotal = 0;
-            bool keepGoing = true;
+            var runningTotal = 0.0;
 
-            while (keepGoing)
+            while (true)
             {
                 // Prompt user for time exercised
                 Console.Write("How many minutes did you workout for? (Type 'quit' if you whimped out, lol) ");
@@ -22,42 +21,40 @@ namespace FitnessApp
                     string entry = Console.ReadLine();
                     if (entry.ToLower() == "quit")
                     {
-                        keepGoing = false;
+                        break;
+                    }
+
+                    // Add time to total
+                    double minutes = double.Parse(entry);
+
+                    if (minutes <= 0)
+                    {
+                        Console.WriteLine("You're a failure...");
+                        Console.WriteLine("but then again you might have just typed something wrong.");
+                        Console.WriteLine("Here try again:");
+                        continue;
+                    }
+                    else if (minutes <= 10)
+                    {
+                        Console.WriteLine("You can do better!");
+                    }
+                    else if (minutes <= 30)
+                    {
+                        Console.WriteLine("Okay, now we're getting somewhere! *clap clap*");
+                    }
+                    else if (minutes <= 60)
+                    {
+                        Console.WriteLine("Feel the BURN! Keep going champ!");
                     }
                     else
                     {
-                        // Add time to total
-                        double minutes = double.Parse(entry);
-
-                        if (minutes <= 0)
-                        {
-                            Console.WriteLine("You're a failure...");
-                            Console.WriteLine("but then again you might have just typed something wrong.");
-                            Console.WriteLine("Here try again:");
-                            continue;
-                        }
-                        else if (minutes <= 10)
-                        {
-                            Console.WriteLine("You can do better!");
-                        }
-                        else if (minutes <= 30)
-                        {
-                            Console.WriteLine("Okay, now we're getting somewhere! *clap clap*");
-                        }
-                        else if (minutes <= 60)
-                        {
-                            Console.WriteLine("Feel the BURN! Keep going champ!");
-                        }
-                        else
-                        {
-                            Console.WriteLine("THIS IS SPARTA!!!");
-                        }
-
-                        runningTotal = runningTotal + minutes;
-
-                        //Display total time
-                        Console.Write("You've worked out for: " + runningTotal + " minutes!");
+                        Console.WriteLine("THIS IS SPARTA!!!");
                     }
+
+                    runningTotal += minutes;
+
+                    //Display total time
+                    Console.Write("You've worked out for: " + runningTotal + " minutes!");
                     Console.WriteLine("See ya!");
                 }
                 catch (FormatException)
