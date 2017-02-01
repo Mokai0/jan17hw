212df45dc395bf2a787f179cd39d253f2d250780
Mohammad Hanbali <mokai86@gmail.com>
2017-02-01 15:35:32 -0500

    uploading

--- TestApps/TestApps/Program.cs
+++ TestApps/TestApps/Program.cs
@@ -10,23 +10,29 @@ namespace TestApps
     {
         static void Main()
         {
+            
             Console.Write("Enter the number of times to print \"Yay!\": ");
+
+            var count = 0;
+
             while (true)
             {
                 try
                 {
                     string entry = Console.ReadLine();
+                    if (entry.ToLower() == "q")
+                    {
+                        break;
+                    }
                     var limit = int.Parse(entry);
-                    var count = 0;
-                    if (count < limit)
+                    while (count < limit)
                     {
                         Console.Write("Yay!");
                         count += 1;
+                        continue;
                     }
-                    else
-                    {
-                        Console.WriteLine("Okay, what now?");
-                    }
+                    count = 0;
+                Console.WriteLine("Okay, what now? (Press q to quit)");
                 }
                 catch (FormatException)
                 {
