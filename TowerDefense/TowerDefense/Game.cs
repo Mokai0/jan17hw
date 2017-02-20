ad71a7f80e28def53b7faebdeefdab19b71a4188
Mohammad Hanbali <mokai86@gmail.com>
2017-02-20 13:22:24 -0500

    Practicing errors and how to trigger them and their messages

--- TowerDefense/TowerDefense/Game.cs
+++ TowerDefense/TowerDefense/Game.cs
@@ -12,13 +12,15 @@ namespace TowerDefense
         {
             Map map = new Map(8, 5);
 
-            Point x = new MapLocation(4, 2);
+            try
+            {
+                MapLocation mapLocation = new MapLocation(20, 20, map);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-            Point p = x;
-
-            map.OnMap(new MapLocation(0, 0));
-
-            Console.WriteLine(x.DistanceTo(5, 5));
             Console.ReadKey();
         }
     }
