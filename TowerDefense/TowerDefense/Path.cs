b0a46b98942a7d1c705f9334dc3cfdf5405044ce
Mohammad Hanbali <mokai86@gmail.com>
2017-02-20 14:27:47 -0500

    Defining the path - doesn't work -.-"

--- TowerDefense/TowerDefense/Path.cs
+++ TowerDefense/TowerDefense/Path.cs
@@ -8,5 +8,23 @@ namespace TowerDefense
 {
     class Path
     {
+        private readonly MapLocation[] _path;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public GetLocationAt(int pathStep)
+        {
+            if(pathStep < _path.length)
+            {
+                return _path[pathStep];
+            }
+            else
+            {
+                return null;
+            }
+        }
     }
 }
