3371a9a6ad1e1bee0379046ce79f5e4d890b24e7
Mohammad Hanbali <mokai86@gmail.com>
2017-02-20 12:59:14 -0500

    I was missing brackets...-.-"

--- TowerDefense/TowerDefense/MapLocation.cs
+++ TowerDefense/TowerDefense/MapLocation.cs
@@ -8,6 +8,9 @@ namespace TowerDefense
 {
     class MapLocation : Point
     {
-        public MapLocation(int x, int y) : base(x,y);
+        public MapLocation(int x, int y) : base(x,y)
+        {
+
+        }
     }
 }
