aa74b7d4725dfbb71dd35c4c11ed42bf12c02cbe
Mohammad Hanbali <mokai86@gmail.com>
2017-02-23 16:14:52 -0500

    Auto Properties

--- TowerDefense/TowerDefense/Invader.cs
+++ TowerDefense/TowerDefense/Invader.cs
@@ -9,6 +9,6 @@ namespace TowerDefense
     class Invader
     {
         public MapLocation Location { get; private set; }
-        //Keeping the setter private is a common practice - it future proofs the code so that you can change the field's contents when working within the same class and ensure it's entgrity out side of the class. At the same time its a good idea to keep the getter public to allow access to the information from outside the class.
+        //Keeping the setter private is a common practice - it future proofs the code so that you can change the field's contents when working within the same class and ensure it's entgrity out side of the class. At the same time its a good idea to keep the getter public to allow access to the information from outside the class. The above is what's known as an AUTO PROPERTY.
     }
 }
