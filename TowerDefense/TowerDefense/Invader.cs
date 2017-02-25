956497bf66db60c3e80f91be58b254d0f05bc665
Mohammad Hanbali <mokai86@gmail.com>
2017-02-24 19:28:08 -0500

    Learning syntactic sugar

--- TowerDefense/TowerDefense/Invader.cs
+++ TowerDefense/TowerDefense/Invader.cs
@@ -11,22 +11,17 @@ namespace TowerDefense
         private readonly Path _path;
         private int _pathStep = 0;
 
-        public MapLocation Location
-        {
-            get
-            {
-                return _path.GetLocationAt(_pathStep);
-            }
-        }        
-        //"private set;" deleted because this is now a computed property and therefore has no setter, private or otherwise.
+        public MapLocation Location => _path.GetLocationAt(_pathStep);
+        //The above is the syntactic sugar for the below code block, so long as it takes a single statement and is used strictly for computed properties this will work!
+        //{ get { return _path.GetLocationAt(_pathStep); } }        
+
         public Invader(Path path)
         {
             _path = path;
         }
 
-        public void Move()
-        {
-            _pathStep += 1;
-        }
+        public void Move() => _pathStep += 1;
+        //Similarly this would be how you write syntactic sugar for single statement methods. Notice how the parentheses, "()", are still there - that's a clear indication of this line being a method. Another difference is that properties will ALWAYS return a value whereas this method, because of 'void', will not.
+        //{ _pathStep += 1; }
     }
 }
