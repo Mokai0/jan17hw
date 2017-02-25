6686cbf59df7613f7cd9f52f63d85ba0946549cc
Mohammad Hanbali <mokai86@gmail.com>
2017-02-24 20:24:54 -0500

    Remember for loops?

--- TowerDefense/TowerDefense/Tower.cs
+++ TowerDefense/TowerDefense/Tower.cs
@@ -8,5 +8,20 @@ namespace TowerDefense
 {
     class Tower
     {
+        private readonly MapLocation _location;
+
+        public Tower(MapLocation location)
+        {
+            _location = location;        
+        }
+
+        public void ShootEm(Invader[] invaders)
+        {
+            for(int i = 0; i <invaders.Length; i++)
+            {
+                Invader invader = invaders[i];
+                //Do stuff with invader
+            }
+        }
     }
 }
