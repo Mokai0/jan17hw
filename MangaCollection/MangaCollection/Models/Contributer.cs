760b761eb03d824e4f173c66b7b5b90f9f3d4433
Mohammad Hanbali <mokai86@gmail.com>
2017-03-16 19:34:39 -0400

    Built the basic models

---
+++ MangaCollection/MangaCollection/Models/Contributer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MangaCollection.Models
+{
+    public class Contributer
+    {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ MangaCollection/MangaCollection/Models/Manga.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MangaCollection.Models
+{
+    public class Manga
+    {
+        public int Id { get; set; }
+        public string SerTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Contributer[] Contributers { get; set; }
+        public bool Favorite { get; set; }
+    }
+}
