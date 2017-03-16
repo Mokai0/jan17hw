ea2638f6386fd68fa3e8b58d939931a009f50a72
Mohammad Hanbali <mokai86@gmail.com>
2017-03-16 19:47:04 -0400

    Updated content as well as Manga model

--- MangaCollection/MangaCollection/Models/Manga.cs
+++ MangaCollection/MangaCollection/Models/Manga.cs
@@ -8,10 +8,28 @@ namespace MangaCollection.Models
     public class Manga
     {
         public int Id { get; set; }
-        public string SerTitle { get; set; }
+        public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
         public Contributer[] Contributers { get; set; }
         public bool Favorite { get; set; }
+
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+
+        //series-title-issuenumber.jpg
+        public string CoverImageFileName
+        {
+            get
+            {
+                return SeriesTitle.Replace("", "-")
+                    .ToLower() + "-" + IssueNumber + ".jpg";
+            }
+        }
     }
 }
