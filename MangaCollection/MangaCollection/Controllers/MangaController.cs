6cfb770e9a1821a50217c7c56f3d17ce405fbbd7
Mohammad Hanbali <mokai86@gmail.com>
2017-03-16 18:48:18 -0400

    Learning about ViewBag an how to use it

--- MangaCollection/MangaCollection/Controllers/MangaController.cs
+++ MangaCollection/MangaCollection/Controllers/MangaController.cs
@@ -10,6 +10,16 @@ namespace MangaCollection.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.SeriesTitle = "Drifters";
+            ViewBag.IssueNumber = 1;
+            ViewBag.Description = "<p>Debut issue of an incredible tale of heroism! The story of Drifters takes place during Japan's Sengoku period (roughly XV to XVII century), a time of constant warring between the island's states, and a natural setting of choice for samurai stories. Young samurai on verge of death suddenly finds himself into another, fantastic world. How? Why? These questions remain unanswered...</p>";
+            ViewBag.Contributers = new string[]
+            {
+                "Written: Kouta Hirano",
+                "Illustrated:Kouta Hirano",
+                "Published: Shonen Gahosha",
+                "Translated: Dark Horse Comics"
+            };
             return View();
         }
     }
