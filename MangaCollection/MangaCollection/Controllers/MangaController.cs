3cf2c56254b7af6f0c3e55a1c0ac2df7c82bb977
Mohammad Hanbali <mokai86@gmail.com>
2017-03-16 01:21:25 -0400

    I'll Be using this version of .NET (4.6.1) seeing as how the newest version (4.6.2) is not functioning properly at this time.

---
+++ MangaCollection/MangaCollection/Controllers/MangaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MangaCollection.Controllers
+{
+    public class MangaController :Controller
+    {
+        public string Detail()
+        {
+            return "Test page!";
+        }
+    }
+}
