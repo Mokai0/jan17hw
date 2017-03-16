db3b789abee58526f6387c7d278149ec49cc66e4
Mohammad Hanbali <mokai86@gmail.com>
2017-03-15 20:20:55 -0400

    Learning how to make counter/inverse dictionaries and best practice methods there in.

--- MorseCodeTranslator/MorseCodeTranslator/MorseCodeTranslator.cs
+++ MorseCodeTranslator/MorseCodeTranslator/MorseCodeTranslator.cs
@@ -70,6 +70,19 @@ namespace MorseCodeTranslator
             {'Ü', "..--"}
         };
 
+        private static Dictionary<string, char> _morseToText = new Dictionary<string, char>();
+
+        static MorseCodeTranslator()
+        {
+            foreach(KeyValuePair<char, string> code in _textToMorse)
+            {
+                //_morseToText[code.Value] = code.Key;
+                //Useful to add new keys to a dictionary however its a catch 22 since it'll overwrite any pre-existing keys that match the new one.
+                _morseToText.Add(code.Value, code.Key);
+                //This on the other hand will only add keys that haven't been added before and throw an exception/error should any duplicates arise.
+            }
+        }
+
         public static string ToMorse(string input)
         {
             List<string> output = new List<string>(input.Length);
