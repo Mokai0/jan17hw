b9a4edf360b2c86f30d7f5eda271967ff3abd17c
Mohammad Hanbali <mokai86@gmail.com>
2017-03-17 19:49:56 -0400

    Finally have a homepage!!!

--- MangaCollection/MangaCollection/App_Start/RouteConfig.cs
+++ MangaCollection/MangaCollection/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MangaCollection
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Manga", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
