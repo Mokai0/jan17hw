ece3ccf56c5b4aeceda58accbbbd3b0507df403e
Mohammad Hanbali <mokai86@gmail.com>
2017-03-16 20:12:53 -0400

    Updated the Manga detail view to be "Strongly Typed"

--- MangaCollection/MangaCollection/Controllers/MangaController.cs
+++ MangaCollection/MangaCollection/Controllers/MangaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MangaCollection.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,17 +11,21 @@ namespace MangaCollection.Controllers
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "Drifters";
-            ViewBag.IssueNumber = 1;
-            ViewBag.Description = "<p>Debut issue of an incredible tale of heroism! The story of Drifters takes place during Japan's Sengoku period (roughly XV to XVII century), a time of constant warring between the island's states, and a natural setting of choice for samurai stories. Young samurai on verge of death suddenly finds himself into another, fantastic world. How? Why? These questions remain unanswered...</p>";
-            ViewBag.Contributers = new string[]
+            var manga = new Manga()
             {
-                "Written: Kouta Hirano",
-                "Illustrated:Kouta Hirano",
-                "Published: Shonen Gahosha",
-                "Translated: Dark Horse Comics"
+                SeriesTitle = "Drifters",
+                IssueNumber = 1,
+                DescriptionHtml = "<p>Debut issue of an incredible tale of heroism! The story of Drifters takes place during Japan's Sengoku period (roughly XV to XVII century), a time of constant warring between the island's states, and a natural setting of choice for samurai stories. Young samurai on verge of death suddenly finds himself into another, fantastic world. How? Why? These questions remain unanswered...</p>",
+                Contributers = new Contributer[]
+                {
+                    new Contributer() {Name = "Kouta Hirano", Role = "Written" },
+                    new Contributer() {Name = "Kouta Hirano", Role = "Illustrated" },
+                    new Contributer() {Name = "Shonen Gahosha", Role = "Published" },
+                    new Contributer() {Name = "Dark Horse Comics", Role = "Translated" },
+                }
             };
-            return View();
+
+            return View(manga);
         }
     }
 }
