e22438cf6a5a4957902091ff2769bca3d6e634f2
Mohammad Hanbali <mokai86@gmail.com>
2017-03-17 18:59:54 -0400

    Updated comic books controller to use the created repository

--- MangaCollection/MangaCollection/Controllers/MangaController.cs
+++ MangaCollection/MangaCollection/Controllers/MangaController.cs
@@ -1,4 +1,5 @@
-﻿using MangaCollection.Models;
+﻿using MangaCollection.Data;
+using MangaCollection.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,22 +10,20 @@ namespace MangaCollection.Controllers
 {
     public class MangaController :Controller
     {
-        public ActionResult Detail()
-        {
-            var manga = new Manga()
-            {
-                SeriesTitle = "Drifters",
-                IssueNumber = 1,
-                DescriptionHtml = "<p>Debut issue of an incredible tale of heroism! The story of Drifters takes place during Japan's Sengoku period (roughly XV to XVII century), a time of constant warring between the island's states, and a natural setting of choice for samurai stories. Young samurai on verge of death suddenly finds himself into another, fantastic world. How? Why? These questions remain unanswered...</p>",
-                Contributers = new Contributer[]
-                {
-                    new Contributer() {Name = "Kouta Hirano", Role = "Written" },
-                    new Contributer() {Name = "Kouta Hirano", Role = "Illustrated" },
-                    new Contributer() {Name = "Shonen Gahosha", Role = "Published" },
-                    new Contributer() {Name = "Dark Horse Comics", Role = "Translated" },
-                }
-            };
+        private MangaRepo _mangaRepo = null;
 
+        public MangaController()
+        {
+            _mangaRepo = new MangaRepo();
+        }
+
+        public ActionResult Detail(int? id)
+        {
+            if (id==null)
+            {
+                return HttpNotFound();
+            }
+            var manga = _mangaRepo.GetManga(id.Value);
             return View(manga);
         }
     }
--- MangaCollection/MangaCollection/Data/MangaRepo.cs
+++ MangaCollection/MangaCollection/Data/MangaRepo.cs
@@ -12,6 +12,7 @@ namespace MangaCollection.Data
         {
             new Manga()
             {
+                Id = 1,
                 SeriesTitle = "Drifters",
                 IssueNumber = 1,
                 DescriptionHtml = "<p>Debut issue of an incredible tale of heroism! The story of Drifters takes place during Japan's Sengoku period (roughly XV to XVII century), a time of constant warring between the island's states, and a natural setting of choice for samurai stories. Young samurai on verge of death suddenly finds himself into another, fantastic world. How? Why? These questions remain unanswered...</p>",
@@ -25,6 +26,7 @@ namespace MangaCollection.Data
             },
             new Manga()
             {
+                Id = 2,
                 SeriesTitle = "Drifters",
                 IssueNumber = 64,
                 DescriptionHtml = "<p>Oh shit! It's the newest issue of Drifters!!!</p>",
@@ -38,6 +40,7 @@ namespace MangaCollection.Data
             },
             new Manga()
             {
+                Id = 3,
                 SeriesTitle = "OnePunch Man",
                 IssueNumber = 1,
                 DescriptionHtml = "<p>In this new action-comedy, everything about a young man named Saitama screams \"AVERAGE,\" from his lifeless expression, to his bald head, to his unimpressive physique. However, this average-looking fellow doesn't have your average problem... He's actually a superhero that's looking for tough opponents! The problem is, every time he finds a promising candidate he beats the snot out of them in one punch. Can Saitama finally find an evil villain strong enough to challenge him? Follow Saitama through his hilarious romps as he searches for new bad guys to challenge!</p>",
