c1d92e098df587be7089a1f8b3d537bfd46cacf6
Mohammad Hanbali <mokai86@gmail.com>
2017-03-25 22:14:57 -0400

    beginning to set up seed

--- ComicBookGalleryModel/ComicBookGalleryModel/DatabaseInitializer.cs
+++ ComicBookGalleryModel/ComicBookGalleryModel/DatabaseInitializer.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace ComicBookGalleryModel
 {
     internal class DatabaseInitializer
+        : DropCreateDatabaseIfModelChanges<Context>
     {
+        protected override void Seed(Context context)
+        {
+            base.Seed(context);
+        }
     }
 }
