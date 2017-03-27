dec8a6e13056529424e658c874eb6111e7e79a57
Mohammad Hanbali <mokai86@gmail.com>
2017-03-27 08:14:44 -0400

    Working with Delegates

---
+++ LINQ_Basics/LINQ_Basics/BirdsExample/BirdRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Basics.BirdsExample
+{
+    public static class BirdRepository
+    {
+        public static List<Bird> birds = new List<Bird>
+        {
+            new Bird {Name="Cardinal", Color="Red", Sightings=3 },
+            new Bird {Name="Dove", Color="White", Sightings=2 },
+            new Bird {Name="Robin", Color="Red", Sightings=5 },
+            new Bird {Name="Blue Jay", Color="Blue", Sightings=1 }
+        };
+    }
+}
---
+++ LINQ_Basics/LINQ_Basics/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_Basics
+{
+    class Program
+    {
+        delegate void SayGreeting(string name);
+
+        public static void SayHello(string name)
+        {
+            Console.WriteLine(string.Format("Hello, {0}", name));
+        }
+
+        public static void SayGoodbye(string name)
+        {
+            Console.WriteLine(string.Format("Later, {0}", name));
+        }
+
+        static void Main(string[] args)
+        {
+            SayGreeting sayGreeting = new SayGreeting(SayHello);
+            Console.WriteLine("What's your name?");
+            string input = Console.ReadLine();
+            sayGreeting(input);
+            Console.ReadLine();
+            sayGreeting = new SayGreeting(SayGoodbye);
+            sayGreeting(input);
+            Console.ReadKey();
+        }
+    }
+}
