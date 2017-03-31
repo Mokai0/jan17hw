e1503108c96dc8ef90e1c21954ba0efa499a8d70
Mohammad Hanbali <mokai86@gmail.com>
2017-03-31 15:28:06 -0400

    incorporating the new data safely

--- dotnet-ef-migrations-1.2/src/ComicBookGalleryModel/ComicBookGalleryModel/Migrations/201703311916389_AddComicBookAverageRatingEntity.cs
+++ dotnet-ef-migrations-1.2/src/ComicBookGalleryModel/ComicBookGalleryModel/Migrations/201703311916389_AddComicBookAverageRatingEntity.cs
@@ -19,6 +19,13 @@ namespace ComicBookGalleryModel.Migrations
                 .PrimaryKey(t => t.Id)
                 .ForeignKey("dbo.ComicBook", t => t.ComicBookId, cascadeDelete: true)
                 .Index(t => t.ComicBookId);
+
+            //Populate the ComicBookAverageRating table:
+            Sql(@"
+                insert into ComicBookAverageRating
+                select Id, AverageRating, getdate() from ComicBook 
+                where AverageRating is not null
+            ");
             
             DropColumn("dbo.ComicBook", "AverageRating");
         }
@@ -26,6 +33,21 @@ namespace ComicBookGalleryModel.Migrations
         public override void Down()
         {
             AddColumn("dbo.ComicBook", "AverageRating", c => c.Decimal(precision: 5, scale: 2));
+
+            //Populate the ComicBookAverageRating column:
+            Sql(@"
+                update cb
+                set cb.AverageRating = cbar.AverageRating
+                from ComicBook cb
+                cross apply (
+                    select top 1 AverageRating, Date
+                    from ComicBookAverageRating 
+                    where ComicBookId = cb.Id
+                    order by Date desc
+                ) as cbar
+            ");
+            //cross apply allows us to evaluate a table valued expression for each row in the ComicBook table.
+
             DropForeignKey("dbo.ComicBookAverageRating", "ComicBookId", "dbo.ComicBook");
             DropIndex("dbo.ComicBookAverageRating", new[] { "ComicBookId" });
             DropTable("dbo.ComicBookAverageRating");
